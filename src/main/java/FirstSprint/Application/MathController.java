be92a29c85b5b54c835dc64411274f1b986b87ae
Gabriel <g_szybiak@wp.pl>
2023-08-30 18:57:59 +0200

    AW2 Homework Part1

--- src/main/java/FirstSprint/Application/MathController.java
+++ src/main/java/FirstSprint/Application/MathController.java
@@ -2,7 +2,6 @@ package FirstSprint.Application;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
