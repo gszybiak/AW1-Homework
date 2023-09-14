7888cc04c04ea80513aab91fae5b1854c3cccc96
Gabriel <g_szybiak@wp.pl>
2023-09-14 10:25:33 +0200

    AW2 Homework part 3

--- src/main/java/FirstSprint/Application/Application.java
+++ src/main/java/FirstSprint/Application/Application.java
@@ -1,4 +1,4 @@
-package FirstSprint.Application;
+package firstSprint.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
