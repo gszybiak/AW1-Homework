dbe34054c8de7a52d9f25e3353717f62a3f07318
Gabriel <g_szybiak@wp.pl>
2023-09-14 10:26:30 +0200

    AW2 Homework part 3

--- src/main/java/FirstSprint/Application/PD2/task2/ApiProxy.java
+++ src/main/java/FirstSprint/Application/PD2/task2/ApiProxy.java
@@ -9,25 +9,30 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 
 @RequestMapping("/api")
 @Controller
 public class ApiProxy {
 
     @GetMapping("/cenyzlota")
-    @ResponseBody
-    public BigDecimal goldPrice(@RequestParam(required = false, defaultValue = "json") String format){
-        BigDecimal price = getGoldPrice();
-        if ("xml".equals(format)) {
-            return price; //nie wiem jak sformatować na xml
+    public ModelAndView goldPrice(@RequestParam(required = false, defaultValue = "json") String format) {
+        String apiUrl = "http://api.nbp.pl/api/cenyzlota/last?format=";
+
+        if ("json".equalsIgnoreCase(format)) {
+            apiUrl += "json";
+        } else if ("xml".equalsIgnoreCase(format)) {
+            apiUrl += "xml";
         } else {
-            return price;
+            apiUrl += "json";
         }
+
+        RedirectView redirectView = new RedirectView(apiUrl);
+        return new ModelAndView(redirectView);
     }
 
     @GetMapping("/2/cenyzlota")
@@ -37,20 +42,38 @@ public class ApiProxy {
         return "Cena złota w obecnej chwili wynosi " + price.toString();
     }
 
-    @GetMapping("/cenyzlota/last/{count}")
+    @GetMapping("/cenyzlota/last")
     @ResponseBody
-    public ArrayList goldPriceLast(@PathVariable Integer count){
-        ArrayList<BigDecimal> price = getGoldPriceLast(count);
-        return price;
+    public ModelAndView goldPriceLast(@RequestParam(required = false, defaultValue = "1") int count,
+                                      @RequestParam(required = false, defaultValue = "json") String format) {
+        String apiUrl = "http://api.nbp.pl/api/cenyzlota/last?";
+        String formatParam = "format=" + format;
+        String countParam = "count=" + count;
+
+        if ("json".equalsIgnoreCase(format)) {
+            apiUrl += "json";
+        } else if ("xml".equalsIgnoreCase(format)) {
+            apiUrl += "xml";
+        } else {
+            apiUrl += "json";
+        }
+
+        apiUrl += formatParam;
+        if (count > 0) {
+            apiUrl += "&" + countParam;
+        }
+
+        RedirectView redirectView = new RedirectView(apiUrl);
+        return new ModelAndView(redirectView);
     }
 
     @GetMapping("/exchangerates/rates/a/chf")
-    @ResponseBody
-    public BigDecimal goldPriceLast(){
-        BigDecimal price = getCHFPrice();
-        return price;
-    }
+    public ModelAndView chfExchangeRate() {
+        String apiUrl = "http://localhost:8080/api/exchangerates/rates/a/chf/";
 
+        RedirectView redirectView = new RedirectView(apiUrl);
+        return new ModelAndView(redirectView);
+    }
 
     private BigDecimal getGoldPrice() {
         String apiUrl = "http://api.nbp.pl/api/cenyzlota";
@@ -64,52 +87,6 @@ public class ApiProxy {
             JSONArray jsonArray = new JSONArray(responseBody);
             JSONObject jsonObject = jsonArray.getJSONObject(0);
             return jsonObject.getBigDecimal("cena");
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private ArrayList<BigDecimal> getGoldPriceLast(Integer count) {
-        String apiUrl = "http://api.nbp.pl/api/cenyzlota/last/" + count + "?format=json";
-
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            HttpGet httpGet = new HttpGet(apiUrl);
-
-            HttpResponse response = httpClient.execute(httpGet);
-            String responseBody = EntityUtils.toString(response.getEntity());
-
-            JSONArray jsonArray = new JSONArray(responseBody);
-            ArrayList<BigDecimal> prices = new ArrayList<>();
-
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject json = jsonArray.getJSONObject(i);
-                BigDecimal price = json.getBigDecimal("cena");
-                prices.add(price);
-            }
-
-            return prices;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private BigDecimal getCHFPrice() {
-        String apiUrl = "http://api.nbp.pl/api/exchangerates/rates/a/chf?format=json";
-
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            HttpGet httpGet = new HttpGet(apiUrl);
-
-            HttpResponse response = httpClient.execute(httpGet);
-            String responseBody = EntityUtils.toString(response.getEntity());
-
-            JSONObject jsonObject = new JSONObject(responseBody);
-            JSONArray ratesArray = jsonObject.getJSONArray("rates");
-
-            JSONObject firstRate = ratesArray.getJSONObject(0);
-            return  firstRate.getBigDecimal("mid");
         }
         catch (IOException e) {
             e.printStackTrace();
